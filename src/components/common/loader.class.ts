15fc8f8dcc67a7404a57e67007e4066c5b6baceb
Azarattum <azarattum@pm.me>
2019-09-18 00:19:44 +0700

    Implemented AJAX loader. Adjusted architecture

---
+++ src/components/common/loader.class.ts
@@ -0,0 +1,50 @@
+/**
+ * Class for async AJAX loading with JSON support.
+ */
+export default class Loader {
+	public urls: Array<string>;
+
+	constructor(urls: Array<string>) {
+		this.urls = urls;
+	}
+
+	public async load(): Promise<Array<any>> {
+		let responses: Array<any> = [];
+		for (const url of this.urls) {
+			const response = await this.request("GET", url);
+			responses.push(response)
+		}
+
+		return [...responses];
+	}
+
+	private async request(method: string, url: string): Promise<any> {
+		return new Promise(function (resolve, reject) {
+			let xhr = new XMLHttpRequest();
+			//Assume json request
+			if (url.replace(new RegExp("/$"), "").endsWith(".json")) {
+				xhr.overrideMimeType("application/json");
+				xhr.responseType = "json";
+			}
+
+			xhr.open(method, url);
+			xhr.onload = function () {
+				if (this.status >= 200 && this.status < 300) {
+					resolve(xhr.response);
+				} else {
+					reject({
+						status: this.status,
+						statusText: xhr.statusText
+					});
+				}
+			};
+			xhr.onerror = function () {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			};
+			xhr.send();
+		});
+	}
+}
--- src/index.include.ts
+++ src/index.include.ts
@@ -1,3 +1,16 @@
-window.addEventListener("load", () => {
-    
+import Loader from "./components/common/loader.class.js";
+
+const URLS = [
+    "sessions.json",
+    // "libs/charts/shaders/bar.fsh",
+    // "libs/charts/shaders/bar.vsh",
+    // "libs/charts/shaders/layout.fsh",
+    // "libs/charts/shaders/layout.vsh"
+];
+
+window.addEventListener("load", async () => {
+    const loader = new Loader(URLS);
+    const data = await loader.load();
+
+    console.log(data);
 });
